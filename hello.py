a2d99889cfd769f817440fb6bf82d9fca756680b
Neel Kishnani <neelkishnani@gmail.com>
2019-05-02 22:11:35 -0700

    Beginning hello python program.

---
+++ hello.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    name = input("Hello there! Welcome to Stanford Splash 2019 :-) What's your name? ")
+    print("Hi there,", name, ". You now know how to use Python within the shell. Cool, right?")
+    pass
